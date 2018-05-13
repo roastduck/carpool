03dd29f24827087a808b0bf46b8d961c32072ce0
roastduck <beantang.tang@gmail.com>
2018-05-12 20:18:13 -0700

    switch to Baidu JS API

--- frontend/index.js
+++ frontend/index.js
@@ -1,31 +1,33 @@
 "use strict"
 
-const util = require("util");
 const backend = require("../node_modules/carpool-backend");
 const apiKey = require("./api-key.js");
 
-function genUrl(param) {
-    return util.format(
-        "%s?ak=%s&width=%d&height=%d&bbox=%d,%d;%d,%d",
-        "http://api.map.baidu.com/staticimage/v2",
-        apiKey,
-        600, // Width
-        600, // Height
-        param.minLng,
-        param.minLat,
-        param.maxLng,
-        param.maxLat
-    );
-}
-
 angular.module('appIndex', [])
     .controller('IndexController', ['$scope', ($scope) => {
-        var param = {
-            minLng: backend.getMinLongitude(),
-            maxLng: backend.getMaxLongitude(),
-            minLat: backend.getMinLatitude(),
-            maxLat: backend.getMaxLatitude()
-        };
-        $scope.mapUrl = genUrl(param);
+        const convertor = new BMap.Convertor();
+        const TRUE_COORD = 1;
+        const BD_COORD = 5;
+        function getPoints(arr, callback) {
+            const ptrs = [];
+            for (var i in arr) {
+                ptrs.push(new BMap.Point(arr[i][0], arr[i][1]));
+            }
+            convertor.translate(ptrs, TRUE_COORD, BD_COORD, (ptrs) => {
+                if (ptrs.status == 0)
+                    callback(ptrs.points);
+                else
+                    console.log("Error: Cannot convert coordinate");
+            });
+        }
+
+        const map = new BMap.Map("map");
+        const minLng = backend.getMinLongitude();
+        const maxLng = backend.getMaxLongitude();
+        const minLat = backend.getMinLatitude();
+        const maxLat = backend.getMaxLatitude();
+        getPoints([[(minLng + maxLng) / 2, (minLat + maxLat) / 2]], (ptrs) => {
+            map.centerAndZoom(ptrs[0], 9);
+        });
     }]);
 
